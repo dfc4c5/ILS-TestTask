4336d06c551a2dd8e74987fb9c033130432cfe2a
Denis Yablokov <dyablokov@riverlogic.com>
2023-04-11 11:45:26 +0400

    BaseLogger & StdLogger separation

--- Project/ILS/ILS_StdLog.cpp
+++ Project/ILS/ILS_StdLog.cpp
@@ -1,96 +1,127 @@
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
+#include <fstream>
+#include <string>
 #include "ILS_StdLog.h"
 
-//=============================================================================
-// BaseLogger - стандартая реализация основных механизмов Logger-а. 
-// Рекомендуется наследоваться именно от этого класса при определении 
-// собственного механизма регистрации хода процесса.
-//-----------------------------------------------------------------------------
-// Конструктор
-BaseLogger::BaseLogger() {
-	// По умолчанию ничего не выводим в заголовке
-	show_info = 0;
-	// Время начала
-	bStarted = false;
-	// флаг вывода лога в консоль
-	bLogToConsole = false;
-};
-//-----------------------------------------------------------------------------
-// Функции интерфейса
-// Регистрация сообщений разных типов, каждая функция имеет праметры:
-// id  - идентификатор сообщений
-// msg - тело сообщения в формате функции printf
-// ... - набор данных для вывода в сообщении по принципу printf
-// Функции:
-// Регистрация информационного сообщения
-void BaseLogger::infOut(const Msg& msg, const LogId& id) const {
-	lOut(iTitle() + msg);
+StdLogger::StdLogger(std::ostream& l_out /*= std::cout*/,
+	std::ostream& w_out /*= std::cerr*/,
+	std::ostream& e_out /*= std::cerr*/) {
+	log_out = &l_out; l_del = false;
+	wrn_out = &w_out; w_del = false;
+	err_out = &e_out; e_del = false;
+	BaseLogger::onLogStart(true, wrn_out != log_out, err_out != wrn_out && err_out != log_out);
 }
-//-----------------------------------------------------------------------------
-// Функции интерфейса
-// Регистрация сообщений разных типов, каждая функция имеет праметры:
-// id  - идентификатор сообщений
-// msg - тело сообщения в формате функции printf
-// ... - набор данных для вывода в сообщении по принципу printf
-// Функции:
-// Регистрация информационного сообщения
-void BaseLogger::logOut(const Msg& msg, const LogId& id) const {
-	lOut(lTitle()+msg);
-}
-// Регистрация предупреждения (warning) и не фатальной ошибки
-void BaseLogger::wrnOut(const Msg& msg, const LogId& id) const {
-	++warnings;
-	wOut(wTitle()+msg);
-}
-// Регистрация фатальной ошибки, 
-// после которой результаты процесса не определены
-void BaseLogger::errOut(const Msg& msg, const LogId& id) const {
-	++errors;
-	eOut(eTitle()+msg);
-}
-//-----------------------------------------------------------------------------
-// Вспомогательные функции
-// Создается общая для всех типов сообщений строка с заголовком
-// на основе значений настроек
-std::string BaseLogger::title() const {
-	std::string res = "";
-	char s[128];
-	time_t ltime;
-	time( &ltime );
-	struct tm *today;
-	today = localtime( &ltime );
-	if(show_info & 1) {
-		strftime(s, 128, "%Y/%m/%d ", today );
-		res += s;
+
+StdLogger::StdLogger(std::string l_out_file,
+	std::ostream& w_out /*= std::cerr*/,
+	std::ostream& e_out /*= std::cerr*/,
+	std::ios_base::openmode mode /*= std::ios_base::out*/) {
+	using namespace std;
+	log_out = NULL; l_del = false;;
+	if (l_out_file != "") {
+		std::ofstream* log_stream = new std::ofstream(l_out_file.c_str(), mode | std::ios_base::out);
+		if (*log_stream) { log_out = log_stream; l_del = true; }
+		else delete log_stream;
 	}
-	if((show_info & 2) || ((show_info & 4) && (!bStarted))) {
-		strftime(s, 128, "%H:%M:%S ", today );
-		res += s;
+	wrn_out = &w_out; w_del = false;
+	err_out = &e_out; e_del = false;
+	BaseLogger::onLogStart(true, wrn_out != log_out, err_out != wrn_out && err_out != log_out);
+}
+
+StdLogger::StdLogger(std::string l_out_file,
+	std::string w_out_file,
+	std::ostream& e_out /*= std::cerr*/,
+	std::ios_base::openmode mode /*= std::ios_base::out*/) {
+	using namespace std;
+	log_out = NULL; l_del = false;;
+	if (l_out_file != "") {
+		std::ofstream* log_stream = new std::ofstream(l_out_file.c_str(), mode | std::ios_base::out);
+		if (*log_stream) { log_out = log_stream; l_del = true; }
+		else delete log_stream;
 	}
-	if (!bStarted) {
-		start_time = std::chrono::steady_clock::now();
-		bStarted = true;
+	wrn_out = NULL; w_del = false;;
+	if (w_out_file != "") {
+		std::ofstream* wrn_stream = new std::ofstream(w_out_file.c_str(), mode | std::ios_base::out);
+		if (*wrn_stream) { wrn_out = wrn_stream; w_del = true; }
+		else delete wrn_stream;
 	}
-	else if (show_info & 4) {
-		long fDurationMilli = long(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time).count());
-		sprintf(s, "% 8.2f ", double(fDurationMilli) / 1000.0);
-		res += s;
+	err_out = &e_out; e_del = false;
+	BaseLogger::onLogStart(true, wrn_out != log_out, err_out != wrn_out && err_out != log_out);
+}
+
+StdLogger::StdLogger(std::string l_out_file,
+	std::string w_out_file,
+	std::string e_out_file,
+	std::ios_base::openmode mode /*= std::ios_base::out*/) {
+	using namespace std;
+	log_out = NULL; l_del = false;;
+	if (l_out_file != "") {
+		std::ofstream* log_stream = new std::ofstream(l_out_file.c_str(), mode | std::ios_base::out);
+		if (*log_stream) { log_out = log_stream; l_del = true; }
+		else delete log_stream;
 	}
-	return res;
+	wrn_out = NULL; w_del = false;;
+	if (w_out_file == l_out_file && w_out_file != "") {
+		wrn_out = log_out;
+		w_del = false;
+	}
+	else if (w_out_file != "") {
+		std::ofstream* wrn_stream = new std::ofstream(w_out_file.c_str(), mode | std::ios_base::out);
+		if (*wrn_stream) { wrn_out = wrn_stream; w_del = true; }
+		else delete wrn_stream;
+	}
+	err_out = NULL; e_del = false;;
+	if (e_out_file == l_out_file && e_out_file != "") {
+		err_out = log_out;
+		e_del = false;
+	}
+	else if (e_out_file == w_out_file && e_out_file != "") {
+		err_out = wrn_out;
+		e_del = false;
+	}
+	else if (e_out_file != "") {
+		std::ofstream* err_stream = new std::ofstream(e_out_file.c_str(), mode | std::ios_base::out);
+		if (*err_stream) { err_out = err_stream; e_del = true; }
+		else delete err_stream;
+	}
+	BaseLogger::onLogStart(true, wrn_out != log_out, err_out != wrn_out && err_out != log_out);
 }
-// Для кажного типа сообзения задается отдельная функция, которая 
-// реализуется на основе общей
-std::string BaseLogger::lTitle() const {
-	return title()+"> ";
+
+StdLogger::StdLogger(const StdLogger& src) {
+	if (src.l_del) log_out = NULL; // не можем прокопировать, иначе возникнет ситуация когда объект родитель умрет и будем иметь указатель на невалидный объект
+	else log_out = src.log_out;
+	if (src.w_del) wrn_out = NULL; // не можем прокопировать, иначе возникнет ситуация когда объект родитель умрет и будем иметь указатель на невалидный объект
+	else wrn_out = src.wrn_out;
+	if (src.e_del) err_out = NULL; // не можем прокопировать, иначе возникнет ситуация когда объект родитель умрет и будем иметь указатель на невалидный объект
+	else err_out = src.err_out;
+	l_del = false;
+	w_del = false;
+	e_del = false;
+	BaseLogger::onLogStart(true, wrn_out != log_out, err_out != wrn_out && err_out != log_out);
 }
-std::string BaseLogger::iTitle() const {
-	return title() + "|INFO> ";
+
+StdLogger::~StdLogger() {
+	BaseLogger::onLogFinish(true, wrn_out != log_out, err_out != wrn_out && err_out != log_out);
+	if (l_del && log_out) delete log_out;
+	if (w_del && wrn_out) delete wrn_out;
+	if (e_del && err_out) delete err_out;
 }
-std::string BaseLogger::wTitle() const {
-	return title()+"|WARNING> ";
+
+void StdLogger::ConsoleOut(const std::string& msg) const {
+	if (bLogToConsole)
+		std::cout << msg << std::endl;
 }
-std::string BaseLogger::eTitle() const {
-	return title()+"|ERROR> ";
+
+void StdLogger::lOut(const std::string& msg) const {
+	if (log_out) (*log_out) << msg << std::endl;
+	ConsoleOut(msg);
+}
+
+void StdLogger::wOut(const std::string& msg) const {
+	if (wrn_out) (*wrn_out) << msg << std::endl;
+	ConsoleOut(msg);
+}
+
+void StdLogger::eOut(const std::string& msg) const {
+	if (err_out) (*err_out) << msg << std::endl;
+	ConsoleOut(msg);
 }
--- Project/ILS/ILS_StdLog.h
+++ Project/ILS/ILS_StdLog.h
@@ -1,104 +1,10 @@
 #pragma once
 
-#include <stdio.h>
-#include <ios>
-#include <iostream>
-#include <fstream>
-#include <chrono>
-#include "ILS_Logger.h"
+#ifndef ILS_StdLoggerH
+#define ILS_StdLoggerH
 
-//=============================================================================
-/// Стандартная реализация большинства методов интерфейса \c ILogger.
-/// @ingroup Kernel
-/// Данный класс реализует некоторые простым стандартным образом основные 
-/// механизмы интрефейса \c ILogger. <i> При реализации механизма регистрации хода процессов, 
-/// удобнее наследоваться именно от \c BaseLogger, а не непосредственно от \c ILogger. </i>
-/// Данный класс реализует:
-/// - формирование заголовка сообщений;
-/// - элементарные настройки формата сообщений.
-/// \see ILogger , StdLogger
-class BaseLogger : public ILogger {
-	//---------------------------------------------------------------------------
-protected: // Управляющие аттрибуты
-	/// Общее количество ошибок зафиксированных этим логгером.
-	mutable unsigned int errors = 0;
-	/// Общее количество предупреждеий зафиксированных этим логгером.
-	mutable unsigned int warnings = 0;
-	//---------------------------------------------------------------------------
-public: // Конструктор
-	BaseLogger();
-	//---------------------------------------------------------------------------
-public: // Настройки StdLogger-а
-	/// Маска битов, определяющая что именно выводить в заголовке.
-	///  - 1 выводить дату
-	///  - 2 выводить время
-	///  - 4 выводить количество секунд с начала
-	mutable unsigned int show_info;
-	/// Флаг того, что стартовали отсчёт времени
-	mutable bool bStarted;
-	/// Флаг того, что нужно выводить лог в консоль
-	mutable bool bLogToConsole;
-	/// Время начала работы.
-	mutable std::chrono::steady_clock::time_point start_time;
-protected: // Функции интерфейса
-	//---------------------------------------------------------------------------
-	// Регистрация сообщения для анализатора логов
-	virtual void infOut(const Msg& msg, const LogId& id) const;
-	// Регистрация информационного сообщения
-	virtual void logOut(const Msg& msg, const LogId& id) const;
-	// Регистрация предупреждения (warning) и не фатальной ошибки
-	virtual void wrnOut(const Msg& msg, const LogId& id) const;
-	// Регистрация фатальной ошибки, после которой результаты процесса не определены
-	virtual void errOut(const Msg& msg, const LogId& id) const;
-protected: // Вспомогательные функции
-	/// Создание строки со стандартным заголовком для сообщения.
-	/// Создается общая для всех типов сообщений строка с заголовком на основе 
-	/// значений настроек.
-	/// \note Эту функцию можно переопределить, в случае необходимости 
-	/// генерировать заголовок сообщения отличный  от стандартного.
-	/// \see log() , wrn() , error() 
-	virtual std::string title() const;
-	/// Создание строки со стандартным заголовком для информационного сообщения.
-	/// Создание строки со стандартным заголовком для информационного сообщения.
-	/// \note Эту функцию можно переопределить, в случае необходимости 
-	/// генерировать заголовок сообщения отличный  от стандартного.
-	virtual std::string lTitle() const;
-	/// Создание строки со стандартным заголовком для информационного сообщения.
-	/// Создание строки со стандартным заголовком для информационного сообщения.
-	/// \note Эту функцию можно переопределить, в случае необходимости 
-	/// генерировать заголовок сообщения отличный  от стандартного.
-	virtual std::string iTitle() const;
-	/// Создание строки со стандартным заголовком для анализатора логов.
-	/// Создание строки со стандартным заголовком для анализатора логов.
-	/// \note Эту функцию можно переопределить, в случае необходимости 
-	/// генерировать заголовок сообщения отличный  от стандартного.
-	virtual std::string wTitle() const;
-	/// Создание строки со стандартным заголовком для ошибки.
-	/// Создание строки со стандартным заголовком для ошибки.
-	/// \note Эту функцию можно переопределить, в случае необходимости 
-	/// генерировать заголовок сообщения отличный  от стандартного.
-	virtual std::string eTitle() const;
-	//---------------------------------------------------------------------------
-protected: // Функиции механизма вывода
-	/// Вывод информационного сообщения.
-	/// Вируальная функция, которая <b>должна быть обязательно переопределена</b>
-	/// как вывод пользователю информационного сообщения, чтобы происходил 
-	/// реальный вывод на экран или в файл или в графическое окно и т.п.
-	/// \param msg - текст информационного сообщения.
-	virtual void lOut(const std::string& msg) const = 0;
-	/// Вывод предупреждения.
-	/// Вируальная функция, которая <b>должна быть обязательно переопределена</b>
-	/// как вывод пользователю предупреждения, чтобы происходил 
-	/// реальный вывод на экран или в файл или в графическое окно и т.п.
-	/// \param msg - текст предупреждения.
-	virtual void wOut(const std::string& msg) const = 0;
-	/// Вывод ошибки.
-	/// Вируальная функция, которая <b>должна быть обязательно переопределена</b>
-	/// как вывод пользователю обшибки, чтобы происходил 
-	/// реальный вывод на экран или в файл или в графическое окно и т.п.
-	/// \param msg - текст ошибки.
-	virtual void eOut(const std::string& msg) const = 0;
-}; //struct BaseLogger
+#include <iostream>
+#include "ILS_BaseLogger.h"
 
 //=============================================================================
 /// Реализация регистратора хода процесса (Логгера) для потоков.
@@ -113,8 +19,8 @@ class StdLogger : public BaseLogger {
 protected:
 	using BaseLogger::bLogToConsole;
 	// Потоки для вывода информационных сообщений, предупрежнений и ошибок
-	mutable std::ostream *log_out, *wrn_out, *err_out;
-	bool l_del,w_del,e_del;
+	mutable std::ostream* log_out, * wrn_out, * err_out;
+	bool l_del, w_del, e_del;
 	//---------------------------------------------------------------------------
 public: // Конструктор
 	/// Конструктор.
@@ -122,14 +28,9 @@ public: // Конструктор
 	/// \param l_out - поток для вывода информационных сообщений.
 	/// \param w_out - поток для вывода предупреждений.
 	/// \param e_out - поток для вывода ошибок.
-	StdLogger(std::ostream& l_out=std::cout,
-	          std::ostream& w_out=std::cerr,
-	          std::ostream& e_out=std::cerr) {
-		log_out = &l_out; l_del=false;
-		wrn_out = &w_out; w_del=false;
-		err_out = &e_out; e_del=false;
-		BaseLogger::onLogStart(true,wrn_out!=log_out,err_out!=wrn_out&&err_out!=log_out);
-	}
+	StdLogger(std::ostream& l_out = std::cout,
+		std::ostream& w_out = std::cerr,
+		std::ostream& e_out = std::cerr);
 
 	//---------------------------------------------------------------------------
 	/// Конструктор по имени файла для инф. сообщений.
@@ -142,21 +43,9 @@ public: // Конструктор
 	/// это значит, что информационные сообщения должны ингорироваться, а выводится 
 	/// только предупреждения и ошибки.
 	StdLogger(std::string l_out_file,
-	          std::ostream& w_out=std::cerr,
-	          std::ostream& e_out=std::cerr,
-			  std::ios_base::openmode mode = std::ios_base::out
-			  ) {
-		using namespace std;
-		log_out = NULL;l_del=false;;
-		if(l_out_file!="") {
-			std::ofstream* log_stream = new std::ofstream(l_out_file.c_str(),mode|std::ios_base::out);
-			if(*log_stream) {log_out = log_stream;l_del=true;}
-			else delete log_stream;
-		}
-		wrn_out = &w_out; w_del=false;
-		err_out = &e_out; e_del=false;
-		BaseLogger::onLogStart(true,wrn_out!=log_out,err_out!=wrn_out&&err_out!=log_out);
-	}
+		std::ostream& w_out = std::cerr,
+		std::ostream& e_out = std::cerr,
+		std::ios_base::openmode mode = std::ios_base::out);
 	//---------------------------------------------------------------------------
 	/// Конструктор по имени файла для инф. сообщений и предупрждений.
 	/// Конструировние Логгера на потоки вывода библиотеки STL.
@@ -168,26 +57,9 @@ public: // Конструктор
 	/// это значит, что сообщения такого типа должны ингорироваться, а выводится 
 	/// будут только другие сообщения.
 	StdLogger(std::string l_out_file,
-	          std::string w_out_file,
-	          std::ostream& e_out=std::cerr,
-			  std::ios_base::openmode mode = std::ios_base::out
-			  ) {
-		using namespace std;
-		log_out = NULL;l_del=false;;
-		if(l_out_file!="") {
-			std::ofstream* log_stream = new std::ofstream(l_out_file.c_str(),mode|std::ios_base::out);
-			if(*log_stream) {log_out = log_stream;l_del=true;}
-			else delete log_stream;
-		}
-		wrn_out = NULL;w_del=false;;
-		if(w_out_file!="") {
-			std::ofstream* wrn_stream = new std::ofstream(w_out_file.c_str(),mode|std::ios_base::out);
-			if(*wrn_stream) {wrn_out = wrn_stream;w_del=true;}
-			else delete wrn_stream;
-		}
-		err_out = &e_out; e_del=false;
-		BaseLogger::onLogStart(true,wrn_out!=log_out,err_out!=wrn_out&&err_out!=log_out);
-	}
+		std::string w_out_file,
+		std::ostream& e_out = std::cerr,
+		std::ios_base::openmode mode = std::ios_base::out);
 	//---------------------------------------------------------------------------
 	/// Конструктор по имени файла для всех сообщений.
 	/// Конструировние Логгера на потоки вывода библиотеки STL.
@@ -199,77 +71,21 @@ public: // Конструктор
 	/// это значит, что сообщения такого типа должны ингорироваться, а выводится 
 	/// будут только другие сообщения.
 	StdLogger(std::string l_out_file,
-	          std::string w_out_file,
-	          std::string e_out_file,
-			  std::ios_base::openmode mode = std::ios_base::out
-			  ) {
-		using namespace std;
-		log_out = NULL;l_del=false;;
-		if(l_out_file!="") {
-			std::ofstream* log_stream = new std::ofstream(l_out_file.c_str(),mode|std::ios_base::out);
-			if(*log_stream) {log_out = log_stream;l_del=true;}
-			else delete log_stream;
-		}
-		wrn_out = NULL;w_del=false;;
-		if(w_out_file==l_out_file && w_out_file!="") {
-			wrn_out = log_out;
-			w_del = false;
-		} else if(w_out_file!="") {
-			std::ofstream* wrn_stream = new std::ofstream(w_out_file.c_str(),mode|std::ios_base::out);
-			if(*wrn_stream) {wrn_out = wrn_stream;w_del=true;}
-			else delete wrn_stream;
-		}
-		err_out = NULL;e_del=false;;
-		if(e_out_file==l_out_file && e_out_file!="") {
-			err_out = log_out;
-			e_del = false;
-		} else if(e_out_file==w_out_file && e_out_file!="") {
-			err_out = wrn_out;
-			e_del = false;
-		} else if(e_out_file!="") {
-			std::ofstream* err_stream = new std::ofstream(e_out_file.c_str(),mode|std::ios_base::out);
-			if(*err_stream) {err_out = err_stream;e_del=true;}
-			else delete err_stream;
-		}
-		BaseLogger::onLogStart(true,wrn_out!=log_out,err_out!=wrn_out&&err_out!=log_out);
-	}
+		std::string w_out_file,
+		std::string e_out_file,
+		std::ios_base::openmode mode = std::ios_base::out);
 	//---------------------------------------------------------------------------
 	/// Конструктор копирования.
-	StdLogger(const StdLogger& src) {
-		if(src.l_del) log_out=NULL; // не можем прокопировать, иначе возникнет ситуация когда объект родитель умрет и будем иметь указатель на невалидный объект
-		else log_out = src.log_out;
-		if(src.w_del) wrn_out=NULL; // не можем прокопировать, иначе возникнет ситуация когда объект родитель умрет и будем иметь указатель на невалидный объект
-		else wrn_out = src.wrn_out;
-		if(src.e_del) err_out=NULL; // не можем прокопировать, иначе возникнет ситуация когда объект родитель умрет и будем иметь указатель на невалидный объект
-		else err_out = src.err_out;
-		l_del=false;
-		w_del=false;
-		e_del=false;
-		BaseLogger::onLogStart(true,wrn_out!=log_out,err_out!=wrn_out&&err_out!=log_out);
-	}
+	StdLogger(const StdLogger& src);
 	//---------------------------------------------------------------------------
-	virtual ~StdLogger() {
-		BaseLogger::onLogFinish(true,wrn_out!=log_out,err_out!=wrn_out&&err_out!=log_out);
-		if(l_del && log_out) delete log_out;
-		if(w_del && wrn_out) delete wrn_out;
-		if(e_del && err_out) delete err_out;
-	}
+	/// Виртуальный деструктор.
+	virtual ~StdLogger();
 	//---------------------------------------------------------------------------
 protected: // Функиции механизма вывода
-	virtual void ConsoleOut(const std::string& msg) const {
-		if (bLogToConsole)
-			std::cout << msg << std::endl;
-	}
-	virtual void lOut(const std::string& msg) const {
-		if (log_out) (*log_out) << msg << std::endl;
-		ConsoleOut(msg);
-	}
-	virtual void wOut(const std::string& msg) const {
-		if (wrn_out) (*wrn_out) << msg << std::endl;
-		ConsoleOut(msg);
-	}
-	virtual void eOut(const std::string& msg) const {
-		if (err_out) (*err_out) << msg << std::endl;
-		ConsoleOut(msg);
-	}
-}; //struct StdLogger
+	virtual void ConsoleOut(const std::string& msg) const;
+	virtual void lOut(const std::string& msg) const;
+	virtual void wOut(const std::string& msg) const;
+	virtual void eOut(const std::string& msg) const;
+}; // class StdLogger
+
+#endif // ILS_StdLoggerH
